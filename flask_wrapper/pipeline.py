b8c526066e08082efd7d82dc2753a935cbe1946d
Hongshan Li <hongshan.li@alectio.com>
2020-04-27 12:02:44 -0700

    minor fix

--- flask_wrapper/pipeline.py
+++ flask_wrapper/pipeline.py
@@ -9,8 +9,6 @@ import sys
 
 
 class Pipeline(object):
-    app = None
-
     def __init__(self, name, train_fn, test_fn, infer_fn):
         self.app = Flask(name)
 
@@ -50,7 +48,7 @@ class Pipeline(object):
         try:
             prd, lbs = self.test_fn(payload)
             return jsonify(
-                    {'status': 200
+                    {'status': 200,
                      'prd': prd,
                      'lbs': lbs
                 })
@@ -74,63 +72,11 @@ class Pipeline(object):
                 'status': 500
                 })
 
-    def run(self, debug=True, host='0.0.0.0'):
+    def __call__(self, debug=False, host='0.0.0.0'):
         self.app.run(debug=debug, host=host)
 
 
-'''
-class Pipeline(object):
-    app = Flask(__name__)  
-    def __init__(self, train_fn, test_fn, infer_fn):
 
-        self.train_fn = train_fn
-        self.test_fn = test_fn
-        self.infer_fn = infer_fn
-
-        # self.app = Flask()
-
-    @app.route('/train', methods=['POST'])
-    def train(self):
-        payload = request.get_json()
-        try:
-            self.train_fn(payload)
-            return jsonify({'status': 200})
-
-        except Exception as e:
-            traceback.print_exceptions(sys.exc_info())
-            return jsonify({'status': 500})
-    
-    @app.route('/test', methods=['POST'])
-    def test(self):
-        payload = request.get_json()
-        try:
-            self.test_fn(payload)
-            return jsonify({'status': 200})
-
-        except Exception as e:
-            traceback.print_exceptions(sys.exc_info())
-            return jsonify({'status': 500})
-            
-    
-    @app.route('/infer', methods=['POST'])
-    def infer(self):
-        payload = request.get_json()
-        try:
-            output = self.infer_fn(payload)
-            return jsonify({
-                'status': 200,
-                'output': output
-                })
-        except Exception as e:
-            traceback.print_exceptions(sys.exc_info())
-            return jsonify({
-                'status': 500
-                })
-    
-    def __call__(self, debug=True):
-        self.app.run(debug=debug, host='0.0.0.0')
-
-'''
 
 
 
